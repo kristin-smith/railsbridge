a34a0f053d3ded4aa74e46a4fa6abd63230b5711
Kristin Smith <kristsmith@gmail.com>
2020-02-29 10:53:50 -0700

    Added vote model, linked to topic model, added upvote route and button to add an upvote on the topic index page

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_26_030012) do
+ActiveRecord::Schema.define(version: 2020_02_29_172321) do
 
   create_table "topics", force: :cascade do |t|
     t.string "title"
     t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "votes", force: :cascade do |t|
+    t.integer "topic_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
